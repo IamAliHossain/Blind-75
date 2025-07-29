273a77d34ac66d75e99d4d35cc8fdadbcdffc165
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-07-29 12:39:32 +0600

    array folder

--- Array/Three_Sum.cpp
+++ Array/Three_Sum.cpp
@@ -2,8 +2,42 @@
     Problem Link: https://leetcode.com/problems/3sum/description/
     Problem Name: 3Sum
     Difficulty  : Medium
-    Time Complexity : O(N * N * log N)
-    Space Complexity: O(N)
+
+
+    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that 
+    i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+    Notice that the solution set must not contain duplicate triplets.
+
+    
+
+    Example 1:
+
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+    Explanation: 
+    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+    The distinct triplets are [-1,0,1] and [-1,-1,2].
+    Notice that the order of the output and the order of the triplets does not matter.
+    Example 2:
+
+    Input: nums = [0,1,1]
+    Output: []
+    Explanation: The only possible triplet does not sum up to 0.
+    Example 3:
+
+    Input: nums = [0,0,0]
+    Output: [[0,0,0]]
+    Explanation: The only possible triplet sums up to 0.
+    
+
+    Constraints:
+
+    3 <= nums.length <= 3000
+    -105 <= nums[i] <= 10^5
+        
 
 */
 
@@ -12,6 +46,8 @@ using namespace std;
 
 /*
     Using Binary Search Technique 
+    Time Complexity : O(N * N * log N)
+    Space Complexity: O(N)
 */
 class Solution {
 public:
@@ -56,10 +92,57 @@ public:
 };
 
 
+/*
+    Using Two Pointer Technique 1
+*/
+
 
 /*
-    Using Two Pointer Technique 
+    TC : O(n*logn) for sorting function + O(n * n) 
+
+    Therefore :
+    Time Complexity : O(n^2)
+    Space Complexity : O(n^2)
+
 */
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++){
+            // skiping duplicates
+            if(i != 0 and nums[i] == nums[i-1])continue;
+            int j = i+1;
+            int k = n-1;
+            // using Two Pointer 
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    vector<int> res ={nums[i], nums[j], nums[k]};
+                    ans.push_back(res);
+                    j++;
+                    k--;
+                    // skip duplicates
+                    while(j<k && nums[j] == nums[j-1])j++;
+                    while(j<k && nums[k] == nums[k+1])k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// using two pointer technique 2
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
