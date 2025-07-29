25bafed155b5dc50fa855cd14b129b8015729a03
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-07-29 19:10:44 +0600

    update code

--- Array/Contains_Duplicate.cpp
+++ Array/Contains_Duplicate.cpp
@@ -10,6 +10,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// Bruteforce Solution
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -18,6 +20,26 @@ public:
             if(nums[i] == nums[i+1]){
                 return true;
             }
+        }
+        return false;
+    }
+};
+
+// Optimal Solution
+
+/*
+    TC : O(N)
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hashSet;
+        for(int i=0; i<nums.size(); i++){
+            if(hashSet.find(nums[i]) != hashSet.end()){ // O(1)
+                return true;
+            }
+            hashSet.insert(nums[i]); // O(1)
         }
         return false;
     }
