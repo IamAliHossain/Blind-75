e1f3246b68eb24365f84160fd7f431571354e89a
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-08-26 12:00:28 +0600

    BFS solution added

--- Tree/Maximum_Depth_of _inary_Tree.cpp
+++ Tree/Maximum_Depth_of _inary_Tree.cpp
@@ -64,3 +64,42 @@ public:
         return solve(root);
     }
 };
+
+
+
+// BFS solution
+
+
+
+
+/*
+
+    BFS Solution
+
+    TC : O(N)
+    SC : O(N)
+
+*/
+
+class Solution {
+public:
+
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int depth = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0; i<sz; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
