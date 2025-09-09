9895f1ea12ac649763aa2583cdd2498ea929704c
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-09-09 10:07:04 +0600

    Number Islands various solution approach added

--- Graph/Number_of_Islands.cpp
+++ Graph/Number_of_Islands.cpp
@@ -2,8 +2,7 @@
     Problem Link : https://leetcode.com/problems/number-of-islands/description/
     Problem Name : 200. Number of Islands
     Difficulty   : Medium
-    Time Complexity : O(N * M) 
-    Space Complexity : O(N * M) for visited array
+
 
 ==================================================================================================
 
@@ -42,6 +41,8 @@
 
 */
 
+/* Most optimal Solution is at the end */
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -71,6 +72,141 @@ public:
                     dfs(i, j, n, m, grid, vis);
                     cnt++;
                 }
+            }
+        }
+        return cnt;
+    }
+};
+
+/*
+
+    TC : O(N * M)
+    SC : O(N * M) for using extra visited vector
+
+    it could be optimized to (1) we'll do in the next code
+
+*/
+
+class Solution {
+public:
+    bool isValid(int i, int j, int n, int m, int dx[], int dy[], vector<vector<char>>& grid, vector<vector<bool>>&visited){
+        if(i >= n || i < 0 || j >= m || j < 0 || grid[i][j] != '1' || visited[i][j]) return false;
+        return true;
+    }
+    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<bool>>&visited){
+        
+        visited[i][j] = true;
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, -1, 0, 1};
+        for(int k=0; k<4; k++){
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+            if(isValid(ni, nj, n, m, dx, dy, grid, visited)){
+                dfs(ni, nj, n, m, grid, visited);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int cnt = 0 ;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j] || grid[i][j] != '1')continue;
+                dfs(i, j, n, m, grid, visited);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+/*
+
+    TC : O(N * M)
+    SC : O(1) for excluding visited vector
+
+
+*/
+
+class Solution {
+public:
+    bool isValid(int i, int j, int n, int m, int dx[], int dy[], vector<vector<char>>& grid, vector<vector<bool>>&visited){
+        if(i >= n || i < 0 || j >= m || j < 0 || grid[i][j] != '1' || visited[i][j]) return false;
+        return true;
+    }
+    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<bool>>&visited){
+        
+        visited[i][j] = true;
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, -1, 0, 1};
+        for(int k=0; k<4; k++){
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+            if(isValid(ni, nj, n, m, dx, dy, grid, visited)){
+                dfs(ni, nj, n, m, grid, visited);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int cnt = 0 ;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j] || grid[i][j] != '1')continue;
+                dfs(i, j, n, m, grid, visited);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+/*--------------solution 3 the most optimal---------------------------------*/
+
+/*
+
+    TC : O(N * M)
+    SC : O(N * M) for using extra visited vector
+
+    it could be optimized to (1) we'll do in the next code
+
+*/
+
+class Solution {
+public:
+    bool isValid(int i, int j, int n, int m, int dx[], int dy[], vector<vector<char>>& grid){
+        if(i >= n || i < 0 || j >= m || j < 0 || grid[i][j] != '1' ) return false;
+        return true;
+    }
+    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid){
+        
+        grid[i][j] = '0';
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, -1, 0, 1};
+        for(int k=0; k<4; k++){
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+            if(isValid(ni, nj, n, m, dx, dy, grid)){
+                dfs(ni, nj, n, m, grid);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int cnt = 0 ;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] != '1')continue;
+                dfs(i, j, n, m, grid);
+                cnt++;
             }
         }
         return cnt;
