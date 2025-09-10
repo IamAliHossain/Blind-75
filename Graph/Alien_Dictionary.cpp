ee38a515b67bbfeb263049d69e14caf1cf1fc689
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-09-10 11:55:51 +0600

    Alien Dictionary Best Solution is added

--- Graph/Alien_Dictionary.cpp
+++ Graph/Alien_Dictionary.cpp
@@ -8,104 +8,127 @@
     Date : 24-05-2025
 ==================================================================================================
 
-There is a new alien language that uses the English alphabet. However, the order of the letters is unknown to you.
+    There is a new alien language that uses the English alphabet. However, the order of the letters 
+    is unknown to you.
 
-You are given a list of strings words from the alien language's dictionary. Now it is claimed that the strings in words are sorted lexicographically by the rules of this new language.
+    You are given a list of strings words from the alien language's dictionary. Now it is claimed 
+    that the strings in words are sorted lexicographically by the rules of this new language.
 
-If this claim is incorrect, and the given arrangement of string in words cannot correspond to any order of letters, return "".
+    If this claim is incorrect, and the given arrangement of string in words cannot correspond to 
+    any order of letters, return "".
 
-Otherwise, return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there are multiple solutions, return any of them.
+    Otherwise, return a string of the unique letters in the new alien language sorted in 
+    lexicographically increasing order by the new language's rules. If there are multiple 
+    solutions, return any of them.
 
 
-Example 1:
+    Example 1:
 
-Input: words = ["wrt","wrf","er","ett","rftt"]
-Output: "wertf"
-Example 2:
+    Input: words = ["wrt","wrf","er","ett","rftt"]
+    Output: "wertf"
+    Example 2:
 
-Input: words = ["z","x"]
-Output: "zx"
-Example 3:
+    Input: words = ["z","x"]
+    Output: "zx"
+    Example 3:
 
-Input: words = ["z","x","z"]
-Output: ""
-Explanation: The order is invalid, so return "".
- 
-Constraints:
+    Input: words = ["z","x","z"]
+    Output: ""
+    Explanation: The order is invalid, so return "".
+    
+    Constraints:
 
-1 <= words.length <= 100
-1 <= words[i].length <= 100
-words[i] consists of only lowercase English letters.
+    1 <= words.length <= 100
+    1 <= words[i].length <= 100
+    words[i] consists of only lowercase English letters.
 
 */
 
 #include<bits/stdc++.h>
 using namespace std;
+//This is using BFS + Topological solution technique
 
-// This code need to be fixed
-// class Solution {
-// public:
-//     bool dfs(int root, unordered_set<int> adj[], vector<int>& vis, vector<int>& callStack, stack<char> &st ){
-//         vis[root] = 1;
-//         callStack[root] = 1;
-//         bool cycle = false;
-//         for(auto &child : adj[root]){
-//             if(child == root || (vis[child] && !callStack[child])) continue;
-//             if(vis[child]) return true;
-//             cycle |= dfs(child, adj, vis, callStack, st);
-//         }
-//         st.push(root + 'a');
-//         callStack[root] = 0;
-//         return cycle;
-//     }
-    
-//     vector<int> topological(int node, unordered_set<int> adj[], vector<bool> &nodes){
-//         vector<int> vis(26, 0), callStack(26);
-//         stack<char> st;
-//         for(int i=0; i<26; i++){
-//             if(nodes[i] && !vis[i]){
-//                 dfs(i, adj, vis, callStack, st);
-//             }
-//         }
-//         vector<int> ans;
-//         while(!st.empty()){
-//             ans.push_back(st.top());
-//             st.pop();
-//         }
-//         return ans;
-//     }
-//     string alienOrder(vector<string>& words) {
+/*
+    N = words.size
+    L = words[i].size
 
-//         unordered_set<int> adj[26];
-//         vector<bool> nodes(26);
+    TC : O(N * L) + O(26) + O(26 * 26 * 25) + O(26) 
+        : O(N * L)
+    SC : O(26 * 26) ~= O(1)
 
-//         for(int i=0; i<words.size()-1; i++){
-//             string s1 = words[i];
-//             string s2 = words[i+1];
-//             for(auto ch : words[i]) nodes[ch - 'a'] = true;
-
-//             bool insertion = true;
-//             for(int j = 0; j<min(s1.size(), s2.size()); j++){
-//                 if(s1[j] != s2[j]){
-//                     adj[s1[j] - 'a'].insert(s2[j] - 'a');
-//                     insertion = false;
-//                     break;
-//                 }
-//             }
-//             if(s1.size() > s2.size() && insertion) return "";
-//         }
-
+*/
+class Solution {
+  public:
+    string findOrder(vector<string> &words) {
         
-//         string s;
-//         vector<int> ans = topological(26, adj, nodes);
-//         for(auto u : ans){
-//             s += u ;
-//         }
-//         return s ;
-//     }
-// };
+        vector<vector<int>> graph(26);
+        vector<int> indegree(26, 0);
+        vector<bool> present(26, false);
+        
+        //Marking all letters present in the current words
+        for(auto word : words){
+            for(auto ch : word){
+                present[ch - 'a'] = true;
+            }
+        }
+        
+        // Building directed graph
+        for(int i=0; i<words.size()-1; i++){ // O(N)
+            
+            string s1 = words[i];
+            string s2 = words[i+1];
+            int k = 0;
+            bool found = false;
+            
+            while(k < min(s1.size(), s2.size())){ // O(L)
+                if(s1[k] != s2[k]){ 
+                    graph[s1[k] - 'a'].push_back(s2[k]-'a');
+                    indegree[s2[k] - 'a']++;
+                    found = true;
+                    break;
+                }
+                k++;
+            }
+            // invalid case like : abc ab, it's violate the condition
+            if(!found && s1.size() > s2.size()) return ""; 
+        }
+        
+        // Doing Topological sort
+        queue<int> q;
+        for(int i=0; i<26; i++){ // O(26)
+            if(present[i] && indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        string ans = "";
+        while(!q.empty()){ // O(26)
+            int curr = q.front();
+            q.pop();
+            ans += (curr + 'a');
+            for(auto neighbor : graph[curr]){ // O(26 * 25)
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0){
+                    q.push(neighbor);
+                }
+            }
+        }
+        int letter_cnt = 0;
+        for(int i=0; i<26; i++){ // O(26)
+            if(present[i]){
+                letter_cnt++;
+            }
+        }
+        // jotogula letter ache tar sob jodi ans string a na pawa jay taile cycle pawa geche
+        // r cycle pawa geche mane dictionary thik kore neya possible na
+        if(ans.size() < letter_cnt) return "";
+        
+        return ans;
+    }
+};
 
-//This is AC code
+
+//This is using DFS solution technique
 
 class Solution {
 public:
